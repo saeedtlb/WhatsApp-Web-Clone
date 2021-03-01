785d3ff4783b3c149563987029486a22ff6c0df6
saeed-tlb <saeedt7879s@gmail.com>
2021-03-01 18:59:49 +0330

    write functionality for clicking start button and if everything goes right redirect to chat section

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,8 +1,44 @@
+import { useState } from 'react'
+// svg
 import { ReactComponent as Welcome } from '../assets/undraw_welcome_3gvl.svg'
-
+// animate
+import { motion } from 'framer-motion'
+// style
 import '../styles/Css/signIn.css'
+// redux
+import { connect, useDispatch } from 'react-redux'
+// action
+import { setUserName } from '../actions'
 
-const SignIn = () => {
+const SignIn = ({ users, history }) => {
+  const [name, setName] = useState('')
+  const [err, setErr] = useState(false)
+
+  const dispatch = useDispatch()
+
+  const variants = {
+    show: { y: 0, height: 'auto' },
+    hide: { y: '-100%', height: 0 }
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+
+    const typedName = name.trim().toLowerCase()
+
+    const exist = typedName !== '' ? users.find((user) => user.name === typedName) : true
+
+    if (exist) {
+      setErr(true)
+      return
+    }
+
+    dispatch(setUserName(typedName))
+    setName('')
+
+    history.push('/chat')
+  }
+
   return (
     <div className="container">
       <div className="sign__in">
@@ -12,12 +48,26 @@ const SignIn = () => {
         <div className="right">
           <div className="wrapper">
             <h1>Welcome to What's App (Desktop)</h1>
-            <form className="sign__in__form">
+            <form className="sign__in__form" onSubmit={submitForm}>
               <section className="username__box">
                 <label htmlFor="username">user name:</label>
                 <br />
-                <input type="text" placeholder="John smith" name="name" id="username" autoComplete="off" />
+                <input
+                  type="text"
+                  placeholder="John smith"
+                  name="name"
+                  id="username"
+                  autoComplete="off"
+                  onChange={(e) => setName(e.target.value)}
+                  onFocus={() => err && setErr(false)}
+                />
               </section>
+
+              <div className="error">
+                <motion.p animate={err ? 'show' : 'hide'} variants={variants}>
+                  {name === '' ? 'Please insert your name' : 'This username has taken before'}
+                </motion.p>
+              </div>
 
               <button type="submit">Start</button>
             </form>
@@ -28,4 +78,8 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+const mapStateToProps = (state) => ({
+  users: state.users
+})
+
+export default connect(mapStateToProps)(SignIn)
