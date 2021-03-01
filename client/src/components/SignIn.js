fc6d6e2bfa73bd172ba6b6e94f974c7f30ce44d1
saeed-tlb <saeedt7879s@gmail.com>
2021-03-01 16:37:34 +0330

    write the structure of signIn page

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { ReactComponent as Welcome } from '../assets/undraw_welcome_3gvl.svg'
+
+import '../styles/Css/signIn.css'
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>signin</h1>
+    <div className="container">
+      <div className="sign__in">
+        <div className="left">
+          <Welcome />
+        </div>
+        <div className="right">
+          <div className="wrapper">
+            <h1>Welcome to What's App (Desktop)</h1>
+            <form className="sign__in__form">
+              <section className="username__box">
+                <label htmlFor="username">user name:</label>
+                <br />
+                <input type="text" placeholder="John smith" name="name" id="username" autoComplete="off" />
+              </section>
+
+              <button type="submit">Start</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
