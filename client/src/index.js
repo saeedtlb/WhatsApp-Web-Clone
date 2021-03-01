85e53323d524a60d6f254faab98820b8b2e25d4a
saeed-tlb <saeedt7879s@gmail.com>
2021-03-01 17:05:09 +0330

    setup redux for whole app

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// Routes
 import Routes from './router'
-
+// global styles
 import './styles/Css/index.css'
+// Redux setup
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducer'
+import initialStore from './store'
 
-ReactDom.render(<Routes />, document.querySelector('#root'))
+const store = createStore(
+  reducer,
+  initialStore,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDom.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.querySelector('#root')
+)
