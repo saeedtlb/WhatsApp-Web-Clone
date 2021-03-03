dba35a9f5a2178970e69ba979f17e0fae9753943
saeed-tlb <saeedt7879s@gmail.com>
2021-03-03 13:38:50 +0330

    create the chat layout with grid

--- client/src/components/chat/ChatPanel/index.js
+++ client/src/components/chat/ChatPanel/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
+// components
+import Navigation from './Navigation'
 // style
 import '../../../styles/Css/chat.css'
 
 const ChatPanel = () => {
   return (
     <div className="chat__panel">
-      <h1>chat</h1>
+      <Navigation />
     </div>
   )
 }
