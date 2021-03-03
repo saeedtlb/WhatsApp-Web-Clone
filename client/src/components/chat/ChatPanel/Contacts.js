7887c524549f4249e06a4dcb4b648844341ae859
saeed-tlb <saeedt7879s@gmail.com>
2021-03-03 16:38:19 +0330

    add unread checkbox and style it

--- client/src/components/chat/ChatPanel/Contacts.js
+++ client/src/components/chat/ChatPanel/Contacts.js
@@ -1,14 +1,22 @@
 import React from 'react'
 // style
 import '../../../styles/Css/contacts.css'
-
+// icon
 import addMore from '../../../styles/icons/addMore.png'
 
 const Contacts = () => {
   return (
     <div className="contacts">
-      <div className="filter">unread</div>
+      <div className="filter">
+        <label>
+          <input type="checkbox" />
+          <span></span>
+        </label>
+        <h4>unread only</h4>
+      </div>
+
       <div className="contacts__box"></div>
+
       <div className="bottom">
         <h2>You've reached the end.</h2>
         <h3>Addmore friends!</h3>
