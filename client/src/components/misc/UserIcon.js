6c09003ca8bb035c82b2b335acb7493d06b1fcf5
saeed-tlb <saeedt7879s@gmail.com>
2021-03-03 18:18:41 +0330

    make it more general to be able reuse it in contacts section too

--- client/src/components/misc/UserIcon.js
+++ client/src/components/misc/UserIcon.js
@@ -1,17 +1,27 @@
 import '../../styles/Css/userIcon.css'
 
-const UserIcon = ({ name, url, additional, status }) => {
+const UserIcon = ({ name, url, status, text, type }) => {
   const randomColor = () => Math.floor(Math.random() * 16777215).toString(16)
 
   return (
-    <div className={`userBox ${additional && additional}`}>
-      <div className="text">
-        <h3>{name}</h3>
-        <span>{status}</span>
+    <div className="userBox">
+      <div className="userBox__left">
+        <div className="icon" style={{ backgroundColor: '#' + randomColor() }}>
+          {url ? <img src={url} alt="user icon" /> : name.slice(0, 2)}
+        </div>
+        <div className="text">
+          <h3>{name}</h3>
+          {status ? <span>{status}</span> : <span>{text}</span>}
+        </div>
       </div>
-      <div className="icon" style={{ backgroundColor: '#' + randomColor() }}>
-        {url ? <img src={url} alt="user icon" /> : name.slice(0, 2)}
-      </div>
+
+      {type === 'message' && (
+        <div className="userBox__right">
+          <span className="time">13:09</span>
+          <br />
+          <span className="unread">2</span>
+        </div>
+      )}
     </div>
   )
 }
