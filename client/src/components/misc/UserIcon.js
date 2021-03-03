0137bbb28723f7f18c7592609711e70fbc3306ff
saeed-tlb <saeedt7879s@gmail.com>
2021-03-03 12:21:45 +0330

    pass more props to this component to show user status and make some change in style

--- client/src/components/misc/UserIcon.js
+++ client/src/components/misc/UserIcon.js
@@ -1,11 +1,14 @@
 import '../../styles/Css/userIcon.css'
 
-const UserIcon = ({ name, url }) => {
+const UserIcon = ({ name, url, additional, status }) => {
   const randomColor = () => Math.floor(Math.random() * 16777215).toString(16)
 
   return (
-    <div className="userBox">
-      <h2>{name}</h2>
+    <div className={`userBox ${additional && additional}`}>
+      <div className="text">
+        <h2>{name}</h2>
+        <span>{status}</span>
+      </div>
       <div className="icon" style={{ backgroundColor: '#' + randomColor() }}>
         {url ? <img src={url} alt="user icon" /> : name.slice(0, 2)}
       </div>
