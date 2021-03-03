353477f3e555d0470322211f97457b370297eb67
saeed-tlb <saeedt7879s@gmail.com>
2021-03-03 14:21:53 +0330

    import the contacts component and use it in the main chat component

--- client/src/components/chat/ChatPanel/index.js
+++ client/src/components/chat/ChatPanel/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // components
 import Navigation from './Navigation'
+import Contacts from './Contacts'
 // style
 import '../../../styles/Css/chat.css'
 
@@ -8,6 +9,7 @@ const ChatPanel = () => {
   return (
     <div className="chat__panel">
       <Navigation />
+      <Contacts />
     </div>
   )
 }
