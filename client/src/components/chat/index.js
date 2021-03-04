681f2d2bc1049c81f74fa55fe4d6da5c6290cef8
saeed-tlb <saeedt7879s@gmail.com>
2021-03-04 18:51:44 +0330

    add authenication --> if user dident enter his name we'll push him back to sign in page else setup socket

--- client/src/components/chat/index.js
+++ client/src/components/chat/index.js
@@ -1,14 +1,32 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+// custom hook
+import useSocket from '../../hook/useSocket'
 // components
 import Header from './Header'
 import ChatPanel from './chatPanel'
+// store
+import { connect } from 'react-redux'
 
-const Chat = () => (
-  <main style={{ height: '100%' }}>
-    <Header />
-    <ChatPanel />
-  </main>
-)
+const Chat = ({ user, history }) => {
+  useEffect(() => {
+    if (!user) {
+      history.push('/')
+      return
+    }
+  }, [user])
 
-export default Chat
+  useSocket(user)
+
+  return (
+    <main style={{ height: '100%' }}>
+      <Header username={user} />
+      <ChatPanel />
+    </main>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  user: state.name
+})
+
+export default connect(mapStateToProps)(Chat)
