f97c066da16273f2638e7c507648186440a3de57
saeed-tlb <saeedt7879s@gmail.com>
2021-03-04 18:46:41 +0330

    setup backend + socket.io

--- server/index.js
+++ server/index.js
@@ -4,17 +4,30 @@ const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const PORT = process.env || 3001
+const PORT = process.env.PORT || 5000
 const io = socketio(server, {
   cors: {
-    origin: 'http://localhost:3000/chat'
+    origin: 'http://localhost:3000'
   }
 })
 
 io.on('connection', (socket) => {
-  const users = []
+  const users = [{ username: 'omid' }, { username: 'baba' }, { username: 'mom' }, { username: 'akbar' }]
 
-  console.log(socket)
+  for (const [id, socket] of io.of('/').sockets) {
+    users.push({
+      userID: socket.id,
+      username: socket.handshake.auth.username
+    })
+  }
+
+  socket.broadcast.emit('user connected', {
+    userID: socket.id,
+    username: socket.handshake.auth.username
+  })
+  socket.emit('users', users)
+
+  socket.on('disconnect', () => console.log('disconnect'))
 })
 
 server.listen(PORT, () => console.log('Server up and running at ' + PORT))
