fee4dd594c7bd913dcf42e1e0bba70d2f1a9553c
saeed-tlb <saeedt7879s@gmail.com>
2021-03-05 14:17:29 +0330

    if user is online show it

--- server/index.js
+++ server/index.js
@@ -11,23 +11,26 @@ const io = socketio(server, {
   }
 })
 
-io.on('connection', (socket) => {
-  const users = [{ username: 'omid' }, { username: 'baba' }, { username: 'mom' }, { username: 'akbar' }]
+io.on('connect', (socket) => {
+  const users = [{ username: 'omid' }, { username: 'baba' }, { username: 'mom' }, { username: 'akbar', online: true }]
 
   for (const [id, socket] of io.of('/').sockets) {
     users.push({
       userID: socket.id,
-      username: socket.handshake.auth.username
+      username: socket.handshake.auth.username,
+      online: true
     })
   }
+
+  console.log('user connected')
+  socket.emit('users', users)
 
   socket.broadcast.emit('user connected', {
     userID: socket.id,
     username: socket.handshake.auth.username
   })
-  socket.emit('users', users)
 
-  socket.on('disconnect', () => console.log('disconnect'))
+  socket.on('disconnect', () => console.log('offline', socket.id))
 })
 
 server.listen(PORT, () => console.log('Server up and running at ' + PORT))
