2f4ee6097ba2518cc1897ba0cc229eb44cd62bde
saeed-tlb <saeedt7879s@gmail.com>
2021-03-08 18:17:18 +0330

    save messages, socket, allusers to store

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,22 +1,35 @@
-import { NAME, SOCKET } from '../actions/type'
+import { NAME, MESSAGES, SOCKET, ALLUSERS } from "../actions/type";
 
 const reducer = (store, action) => {
-  console.log(action)
+  console.log(action);
 
   switch (action.type) {
     case NAME:
       return {
         ...store,
-        name: action.name
-      }
+        username: action.name,
+      };
+    case MESSAGES:
+      return {
+        ...store,
+        messages: {
+          ...store.messages,
+          [action.payload.chatName]: action.payload.messages,
+        },
+      };
     case SOCKET:
       return {
         ...store,
-        socket: action.socket
-      }
+        socket: action.socket,
+      };
+    case ALLUSERS:
+      return {
+        ...store,
+        allUsers: action.allUsers,
+      };
     default:
-      return store
+      return store;
   }
-}
+};
 
-export default reducer
+export default reducer;
