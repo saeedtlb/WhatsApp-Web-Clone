506cb4f19811c76e51b3f88e81abaf4b6e543522
saeed-tlb <saeedt7879s@gmail.com>
2021-03-08 18:14:09 +0330

    finish socket logic for back-end

--- server/index.js
+++ server/index.js
@@ -1,36 +1,57 @@
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const PORT = process.env.PORT || 5000
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 8080;
 const io = socketio(server, {
   cors: {
-    origin: 'http://localhost:3000'
-  }
-})
+    origin: "http://localhost:3000",
+  },
+});
 
-io.on('connect', (socket) => {
-  const users = [{ username: 'omid' }, { username: 'baba' }, { username: 'mom' }, { username: 'akbar', online: true }]
+let users = [];
+const messages = {
+  general: [],
+};
 
-  for (const [id, socket] of io.of('/').sockets) {
+io.on("connection", (socket) => {
+  socket.on("join", (username) => {
     users.push({
-      userID: socket.id,
-      username: socket.handshake.auth.username,
-      online: true
-    })
-  }
+      _id: socket.id,
+      username,
+    });
+    io.emit("new user", users);
+  });
 
-  console.log('user connected')
-  socket.emit('users', users)
+  socket.on("join room", (roomName, cb) => {
+    socket.join(roomName);
+    cb(messages[roomName], roomName);
+  });
 
-  socket.broadcast.emit('user connected', {
-    userID: socket.id,
-    username: socket.handshake.auth.username
-  })
+  socket.on("send message", ({ content, to, sender, chatName, isChannel }) => {
+    const payload = {
+      content,
+      sender,
+      chatName: isChannel ? chatName : sender,
+    };
+    socket.to(to).emit("new message", payload);
 
-  socket.on('disconnect', () => console.log('offline', socket.id))
-})
+    if (messages[chatName]) {
+      messages[chatName].push({ content, sender });
+    }
+    console.log(users);
+  });
 
-server.listen(PORT, () => console.log('Server up and running at ' + PORT))
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user._id !== socket.id);
+    io.emit("new user", users);
+  });
+});
+
+app.get("/", (req, res) =>
+  res.status(200).write("Back-End for whats app chat messaging")
+);
+
+server.listen(PORT, () => console.log("Server up and running at " + PORT));
