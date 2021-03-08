be70b1d41343e9406327b22dd2623310966ea4b1
saeed-tlb <saeedt7879s@gmail.com>
2021-03-08 18:22:09 +0330

    send message to server and render messages in view box

--- client/src/components/chat/chatPanel/Message.js
+++ client/src/components/chat/chatPanel/Message.js
@@ -1,22 +1,94 @@
-import React from 'react'
+import React, { useState, useMemo } from "react";
 // style
-import '../../../styles/Css/message.css'
+import "../../../styles/Css/message.css";
+// redux Store
+import { connect, useDispatch } from "react-redux";
+import { setMessages } from "../../../actions";
+// custom hook
+import { useSocket } from "../../../hook/useSocket";
 
-const Message = () => {
+const Message = ({
+  currentChat: { isChannel, chatName, reciever_id },
+  username,
+  messages,
+  //   socket,
+}) => {
+  const [message, setMessage] = useState("");
+  const [sendMessage] = useSocket();
+  const dispatch = useDispatch();
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
+    const content = message.trim();
+
+    // is empty?
+    if (!content) return;
+
+    // send message information to server
+    const payload = {
+      content,
+      chatName,
+      isChannel,
+      sender: username,
+      to: isChannel ? chatName : reciever_id,
+    };
+    sendMessage(payload);
+
+    // create a new messages to store
+    const newMessages = [
+      ...messages[chatName],
+      {
+        sender: username,
+        content,
+      },
+    ];
+    dispatch(setMessages(newMessages, chatName));
+
+    // clear the message box
+    setMessage("");
+  };
+
+  //   const renderMessages = useMemo(() => console.log(messages, chatName), [
+  //     messages[chatName],
+  //   ]);
+
+  const renderMessages = useMemo(
+    () =>
+      messages[chatName].map((_message, i) => (
+        <div
+          key={i}
+          className={`${_message.sender === username ? "myself" : "other"}`}
+        >
+          {_message.content}
+        </div>
+      )),
+    [messages[chatName]]
+  );
+
   return (
     <div className="message">
-      <div className="texts">
-        <div className="myself">hi, dear</div>
-        <div className="other">what's up bro???</div>
-      </div>
+      <div className="message__texts">{renderMessages}</div>
       <div className="communication">
-        <form>
-          <textarea name="message" />
-          <button />
+        <form onSubmit={handleSendMessage}>
+          <input
+            type="text"
+            name="message"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <button type="submit" />
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+// send store values as props to component
+const mapStateToProps = (state) => ({
+  currentChat: state.currentChat,
+  username: state.username,
+  messages: state.messages,
+});
+
+export default connect(mapStateToProps)(Message);
