de682f61b80e6ac35e9e7228a7cf25208faf59a4
saeed-tlb <saeedt7879s@gmail.com>
2021-03-09 20:02:25 +0330

    wrapp whole Routes in swith and wrapp it into a container to animate them

--- client/src/router.js
+++ client/src/router.js
@@ -1,15 +1,22 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
-import SignIn from './components/SignIn'
-import Chat from './components/chat'
+import { Route, Switch } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+// components
+import SignIn from "./components/SignIn";
+import Chat from "./components/chat";
+// animation
+import { AnimatePresence } from "framer-motion";
 
 const Routes = () => {
-  return (
-    <Router>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/chat" component={Chat} />
-    </Router>
-  )
-}
+  const location = useLocation();
 
-export default Routes
+  return (
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <Switch location={location} key={location.pathname}>
+        <Route path="/" exact component={SignIn} />
+        <Route path="/chat" component={Chat} />
+      </Switch>
+    </AnimatePresence>
+  );
+};
+
+export default Routes;
