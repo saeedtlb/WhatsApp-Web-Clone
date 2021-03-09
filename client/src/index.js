4f265680a57089932ce10d2205d87c827baac9eb
saeed-tlb <saeedt7879s@gmail.com>
2021-03-09 20:01:17 +0330

    wrapp BrowserRouter here instead of Routes component since there we need to use useLocation hook

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 // Routes
-import Routes from './router'
+import Routes from "./router";
 // global styles
-import './styles/Css/index.css'
+import "./styles/Css/index.css";
 // Redux setup
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './reducer'
-import initialStore from './store'
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducer";
+import initialStore from "./store";
 
 const store = createStore(
   reducer,
   initialStore,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+);
 
 ReactDom.render(
   <Provider store={store}>
-    <Routes />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   </Provider>,
-  document.querySelector('#root')
-)
+  document.querySelector("#root")
+);
