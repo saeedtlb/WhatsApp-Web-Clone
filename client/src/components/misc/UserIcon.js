c606341b6100435b8db5e364d93872c0b3fd36d4
saeed-tlb <saeedt7879s@gmail.com>
2021-03-11 17:07:32 +0330

    show the last text dynamically so as time of the message

--- client/src/components/misc/UserIcon.js
+++ client/src/components/misc/UserIcon.js
@@ -1,29 +1,36 @@
-import '../../styles/Css/userIcon.css'
+import "../../styles/Css/userIcon.css";
 
-const UserIcon = ({ name, url, status, text, type, online }) => {
-  const randomColor = () => Math.floor(Math.random() * 16777215).toString(16)
+const UserIcon = ({ name, url, status, text, type, isChannel }) => {
+  const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
 
   return (
     <div className="userBox">
       <div className="userBox__left">
-        <div className={`icon ${online && 'online'}`} style={{ backgroundColor: '#' + randomColor() }}>
+        <div
+          className="icon online"
+          className={`icon ${
+            type === "message" && !isChannel ? "online" : null
+          }`}
+          style={{ backgroundColor: "#" + randomColor() }}
+        >
           {url ? <img src={url} alt="user icon" /> : name.slice(0, 2)}
         </div>
         <div className="text">
           <h3>{name}</h3>
-          {status ? <span>{status}</span> : <span>{text}</span>}
+          {status ? <span>{status}</span> : null}
+          {text ? <span>{text.content}</span> : null}
         </div>
       </div>
 
-      {type === 'message' && (
+      {type === "message" && (
         <div className="userBox__right">
-          <span className="time">13:09</span>
+          <span className="time">{text ? text.time : null}</span>
           <br />
           <span className="unread">2</span>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default UserIcon
+export default UserIcon;
