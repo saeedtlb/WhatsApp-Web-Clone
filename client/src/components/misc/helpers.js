4636c8db76132554477f985d679b17ec7a4d555b
saeed-tlb <saeedt7879s@gmail.com>
2021-04-13 13:40:05 +0430

    export a helper function which gets emoji unicode and turn it to emoji shape

--- client/src/components/misc/helpers.js
+++ client/src/components/misc/helpers.js
@@ -9,3 +9,8 @@ export const handleBackSpace = e => {
     }
   }
 };
+
+export const onEmojiClick = ({ unicode }) => {
+  const codePoints = unicode.split("-").map(u => parseInt(u, 16));
+  return String.fromCodePoint.apply(String, codePoints);
+};
