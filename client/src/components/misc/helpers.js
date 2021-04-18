57c5b0d3b56a01b3d8d5af18914e5ef1ea056274
saeed-tlb <saeedt7879s@gmail.com>
2021-04-18 12:31:57 +0430

    remove the handleBackSpace function and add it to MessageForm component

--- client/src/components/misc/helpers.js
+++ client/src/components/misc/helpers.js
@@ -1,15 +1,3 @@
-export const handleBackSpace = e => {
-  if (e.key === "Backspace") {
-    const { rows, value } = e.target;
-
-    const lines = value.split("\n");
-
-    if (lines[lines.length - 1] === "") {
-      e.target.rows = rows > 2 ? rows - 1 : rows;
-    }
-  }
-};
-
 export const onEmojiClick = ({ unicode }) => {
   const codePoints = unicode.split("-").map(u => parseInt(u, 16));
   return String.fromCodePoint.apply(String, codePoints);
